235ea93b87841191e2beb1154d94b1bf8f28b54c
diogo262 <thenolance10@gmail.com>
2024-03-22 10:36:39 -0300

    Refatorada a Classe PessoaController

--- projetos/AppListaCurso/app/src/main/java/devandroid/diogo/applistacurso/controller/PessoaController.java
+++ projetos/AppListaCurso/app/src/main/java/devandroid/diogo/applistacurso/controller/PessoaController.java
@@ -1,13 +1,24 @@
 package devandroid.diogo.applistacurso.controller;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import devandroid.diogo.applistacurso.model.Pessoa;
+import devandroid.diogo.applistacurso.view.MainActivity;
 
 public class PessoaController {
 
+    SharedPreferences preferences;
+    SharedPreferences.Editor listaVip;
+    public static final String NOME_PREFERENCES = "pref_listavip";
+
+    public PessoaController(MainActivity mainActivity) {
+        preferences = mainActivity.getSharedPreferences(NOME_PREFERENCES, 0);
+        listaVip = preferences.edit();
+    }
 
     @NonNull
     @Override
@@ -18,8 +29,34 @@ public class PessoaController {
         return super.toString();
     }
 
+
     public void salvar(Pessoa pessoa) {
-        Log.d("MVC_Controller", "Salvo: "+pessoa.toString());
+        Log.d("MVC_Controller", "Salvo: " + pessoa.toString());
+
+        listaVip.putString("primeiroNome", pessoa.getPrimeiroNome());
+        listaVip.putString("sobreNome", pessoa.getSobreNome());
+        listaVip.putString("nomeCurso", pessoa.getCursoDesejado());
+        listaVip.putString("telefoneContato", pessoa.getTelefoneContato());
+        listaVip.apply();
+
+    }
+
+    public Pessoa buscar(Pessoa pessoa) {
+
+        pessoa.setPrimeiroNome(preferences.getString("primeiroNome", "NA"));
+        pessoa.setSobreNome(preferences.getString("sobreNome", "NA"));
+        pessoa.setCursoDesejado(preferences.getString("nomeCurso", "NA"));
+        pessoa.setTelefoneContato(preferences.getString("telefoneContato", "NA"));
+
+
+        return pessoa;
+
+    }
+
+    public void limpar() {
+
+        listaVip.clear();
+        listaVip.apply();
 
     }
 }
